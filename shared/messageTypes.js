02fe30fd8716a2306b13d151c2d6810eb365c055
Федор Жеков <zhekov_fi@pik.ru>
2018-02-14 12:08:34 +0300

    extracted string constants

---
+++ shared/messageTypes.js
@@ -0,0 +1,1 @@
+export const EXPORT_COMPLETE = 'EXPORT_COMPLETE';
---
+++ shared/queueNames.js
@@ -0,0 +1,8 @@
+export const RVT_TASK_EXCHANGE = 'extractRvt';
+export const COMPLETE_RVT_EXCHANGE = 'rvt';
+export const COMPLETE_RVT_QUEUE = 'completeRvt';
+export const ERROR_RVT_QUEUE = 'errorRvt';
+export const NWC_TASK_EXCHANGE = 'convertNwc';
+export const COMPLETE_NWC_EXCHANGE = 'nwc';
+export const COMPLETE_NWC_QUEUE = 'completeNwv';
+export const ERROR_NWC_QUEUE = 'errorNWC';
